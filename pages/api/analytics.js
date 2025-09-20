345e300ddadcf5d98ca8d6c1411b9efb8d289cc0
Rafiza-Fried-Chicken <mangvios0@gmail.com>
2025-09-20 21:13:11 +0700

    Delete pages directory

--- pages/api/analytics.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  // Dummy data analytics
-  res.status(200).json({
-    saldo: 10000,
-    totalSent: 100,
-    botConnected: 1,
-    botDisconnected: 0,
-  });
-}
